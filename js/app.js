590aaadf2b7892a22a0d144c966ac737f6c4a2c5
Abi Vickery <vickeryabi@gmail.com>
2019-11-04 13:29:25 -0600

    psuedo code
    a bit of psuedo code as a sort of plan

--- js/app.js
+++ js/app.js
@@ -2,5 +2,12 @@
 function age1() {
     console.log("what is your age?");
     document.write("Please enter your age.");
-    
+    // if age > 5 
+    //     then print('What is a person of your age doing here?');
+    // else if age >= 10
+    //     then print("I'm sorry, but you are not old enough.");
+    // else if age >= 15
+    //     then print('Sorry, but you are still a bit too young, maybe a little older.');
+    // else if age <= 18
+    //     then print('Yes, hello!')
 }

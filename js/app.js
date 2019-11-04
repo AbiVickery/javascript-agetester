8b6ceb0d4dea010732bc02e156051186065287dd
Abi Vickery <vickeryabi@gmail.com>
2019-11-04 14:57:36 -0600

    stuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.write('hello world');
-function age1() {
-    console.log("what is your age?");
-    document.write("Please enter your age.");
+// function age1() {
+    // console.log("what is your age?");
+    // document.write("Please enter your age.");
     // if age > 5 
     //     then print('What is a person of your age doing here?');
     // else if age >= 10
@@ -10,4 +10,8 @@ function age1() {
     //     then print('Sorry, but you are still a bit too young, maybe a little older.');
     // else if age <= 18
     //     then print('Yes, hello!')
-}
+// }
+
+console.log("What is your age?");
+document.write("Please enter your age.");
+Input()
